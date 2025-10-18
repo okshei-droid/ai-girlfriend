8ef8b574d4db2c1e555812932603d1ea7b351320
okshei-droid <okshei@gmail.com>
2025-10-18 12:06:37 +0200

    setup: tailwind + globals css

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        luna: { bg: "#fffafc", tint: "#f6e7f1", ink: "#1f1f1f" }
-      },
+      colors: { luna: { bg: "#fffafc", tint: "#f6e7f1", ink: "#1f1f1f" } },
       borderRadius: { xl2: "1.25rem" }
     },
   },
